173d42dd977440c65718ee52b8c8aad6287b2c83
yuri945 <yurig9925@gmail.com>
2026-09-10 20:09:10 -0300

    alterações na aula 6

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-path("", views.inicio, name="inicio"),
-path("tarefas/", views.lista_tarefas, name="lista_tarefas"),
+    path("", views.inicio, name="inicio"),
+    path("tarefas/", views.lista_tarefas, name="lista_tarefas"),
 ]
